e5249022c8695d51b382e30da86a685f8c54006e
zarlykovbaiel <zarlykovbaiel@gmail.com>
2024-04-04 13:46:44 +0600

    изменен файл main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,169 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        "rhymer",
-      )),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            pinned: true,
+            snap: true,
+            floating: true,
+            title: Text("Rhymer"),
+            surfaceTintColor: Colors.transparent,
+            bottom: PreferredSize(
+                preferredSize: Size.fromHeight(70), child: SearchButton()),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 100,
+              child: ListView.separated(
+                padding: const EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 16,
+                ),
+                itemBuilder: ((context, index) {
+                  final rhyme = List.generate(4, (index) => "Рифма $index");
+                  return RhymeHistoryCard(rhyme: rhyme);
+                }),
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          SliverList.builder(
+              itemBuilder: ((context, index) => const RhymeListCard())),
+        ],
+      ),
+    );
+  }
+}
+
+class RhymeHistoryCard extends StatelessWidget {
+  const RhymeHistoryCard({
+    super.key,
+    required this.rhyme,
+  });
+
+  final List<String> rhyme;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BaseContainer(
+      width: 200,
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Cлово",
+            style: theme.textTheme.bodyLarge
+                ?.copyWith(fontWeight: FontWeight.w700),
+          ),
+          Wrap(
+              children: rhyme
+                  .map((e) => Padding(
+                      padding: const EdgeInsets.only(right: 4), child: Text(e)))
+                  .toList())
+        ],
+      ),
+    );
+  }
+}
+
+class BaseContainer extends StatelessWidget {
+  const BaseContainer({
+    super.key,
+    required this.child,
+    required this.width,
+    this.margin,
+    this.padding = const EdgeInsets.only(left: 12),
+  });
+
+  final double width;
+  final EdgeInsets? margin;
+  final EdgeInsets padding;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      width: width,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: theme.cardColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+}
+
+class RhymeListCard extends StatelessWidget {
+  const RhymeListCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BaseContainer(
+      margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 10),
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Рифма", style: theme.textTheme.bodyLarge),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite, color: theme.hintColor.withOpacity(0.2)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchButton extends StatelessWidget {
+  const SearchButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 8),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          color: theme.hintColor.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(16)),
+      child: Row(
+        children: [
+          const Icon(Icons.search),
+          const SizedBox(
+            width: 12,
+          ),
+          Text(
+            "Поиск рифм...",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: theme.hintColor.withOpacity(0.4),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
